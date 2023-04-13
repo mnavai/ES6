668cea66c02270b79e74899eb92cc3fcbf3bcbd7
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-13 15:57:45 -0400

    Update 05_modules.test.js

--- exercises/05_modules.test.js
+++ exercises/05_modules.test.js
@@ -1,6 +1,7 @@
 import * as Mathy from '../common/Mathy'
 import * as IndexImport from '../common'
-// WRITE YOUR IMPORT STATEMENTS HERE
+import _ from 'lodash'
+import {sqrt as mySqrt, square as mySquare} from '../common/Mathy'
 
 test('can import Mathy', () => {
   // this one's already done! You're welcome :)
@@ -10,7 +11,6 @@ test('can import Mathy', () => {
 })
 
 test('can specify what to import, to only retain pieces of the import', () => {
-  // Import `Mathy` again, but pull out only the `sqrt` as mySqrt, and `square` as mySquare
   expect(mySqrt).toBeDefined()
   expect(mySquare).toBeDefined()
   expect(mySqrt).toBe(Mathy.sqrt)
@@ -18,16 +18,12 @@ test('can specify what to import, to only retain pieces of the import', () => {
 })
 
 test('can import from my node_modules', () => {
-  // import `lodash`
   expect(_).toBeDefined()
 })
 
 //////// Elaboration & Feedback /////////
-/*
-http://ws.kcd.im/?ws=ES6+and+Beyond&e=Modules&em=
-*/
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true
   expect(true).toBe(submitted)
 })
 ////////////////////////////////
@@ -36,10 +32,10 @@ test('I submitted my elaboration and feedback', () => {
 test.skip('Index import', () => {
   //I have noticed that using index.js is pretty common pattern
   //If someone has been confused about that maybe this helps
-  expect(IndexImport.variable1).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable2).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable3).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable4).toBe(/* ENTER YOUR GUESS HERE */)
+  expect(IndexImport.variable1).toBe('Bob')
+  expect(IndexImport.variable2).toBe('Kent')
+  expect(IndexImport.variable3).toBe(222)
+  expect(IndexImport.variable4).toBe(false)
 })
 // If you get this far, try adding a few more tests,
 // then file a pull request to add them to the extra credit!

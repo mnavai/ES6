ebfdc5ae200bc6bab3f21912e3e82834bb4e63f8
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-13 15:57:09 -0400

    Update 04_destructure.test.js

--- exercises/04_destructure.test.js
+++ exercises/04_destructure.test.js
@@ -21,23 +21,19 @@ function getNestedNumbers() {
 }
 
 test('can be used to pull apart objects', () => {
-  // Using destructuring, call `getAddress()` and create a 'city', 'state' and 'zip' variable.
-  // const address = getAddress();
-  // const city = address.city;
-  // const state = address.state;
-  // const zip = address.zip;
+  const {city, state, zip} = getAddress()
   expect(city).toBe('Salt Lake City')
   expect(state).toBe('UT')
   expect(zip).toBe(84115)
 })
 
 test('sets missing values to undefined', () => {
-  // Using destructuring, call `getAddress()` and create an 'address' variable.
+  const {address} = getAddress()
   expect(address).toBeUndefined()
 })
 
 test('can alias destructured variables', () => {
-  // Using destructuring, call `getAddress()` and pull the city, state and zip out, and alias them to c, s, z, respectively
+  const {city: c, state: s, zip: z} = getAddress()
   expect(c).toBe('Salt Lake City')
   expect(s).toBe('UT')
   expect(z).toBe(84115)
@@ -47,27 +43,29 @@ test('can alias destructured variables', () => {
 })
 
 test('can destructure nested variables', () => {
-  // Using destructuring, call `getAddress()` and create `lat` and `long` variables.
+  const {
+    coords: {lat, long},
+  } = getAddress()
   expect(lat).toBe(40.776608)
   expect(long).toBe(-111.920485)
   expect(() => noop(coords)).toThrow()
 })
 
 test('can be used to pull apart arrays', () => {
-  // Call getNumbers and pull the first value out as `one` and the second as `two`
+  const [one, two] = getNumbers()
   expect(one).toBe(1)
   expect(two).toBe(2)
 })
 
 test('can skip indexes in arrays', () => {
-  // Call getNumbers and pull the first value out as `one` and the third as `three`
+  const [one, , three] = getNumbers()
   expect(one).toBe(1)
   expect(three).toBe(3)
   expect(() => noop(two)).toThrow()
 })
 
 test('can reach nested arrays', () => {
-  // Call getNestedNumbers and pull the first value out as `one`, the 3 as `three` and 6 as `six`.
+  const [one, , [three, , [, six]]] = getNestedNumbers()
   expect(one).toBe(1)
   expect(three).toBe(3)
   expect(six).toBe(6)
@@ -76,11 +74,8 @@ test('can reach nested arrays', () => {
 // MORE AT http://www.2ality.com/2015/01/es6-destructuring.html
 
 //////// Elaboration & Feedback /////////
-/*
-http://ws.kcd.im/?ws=ES6+and+Beyond&e=Destructuring&em=
-*/
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true
   expect(true).toBe(submitted)
 })
 ////////////////////////////////

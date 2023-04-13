04d6eef431ebe2b9853e83604a342b2322082d64
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-13 15:58:18 -0400

    Update 06_object-literal.test.js

--- exercises/06_object-literal.test.js
+++ exercises/06_object-literal.test.js
@@ -1,14 +1,13 @@
 test('can use shorthand for property names', () => {
   function createMonster(name, power) {
-    // Using NEW Object Literal Syntax, return a literal that will allow the tests to pass
-    // return {
-    //   type: 'Monster',
-    //   name: name,
-    //   power: power,
-    //   attack: function (target){
-    //     return `${this.name} attacked ${target.name}`;
-    //   }
-    // }
+    return {
+      type: 'Monster',
+      name,
+      power,
+      attack(target) {
+        return `${this.name} attacked ${target.name}`
+      },
+    }
   }
 
   const godzilla = createMonster('Godzilla', 1000)
@@ -23,8 +22,7 @@ test('can use expressions as property names', () => {
     return {
       tasty: true,
       type,
-      // add a expression as property name where the property name is the given type.toUpperCase() + type.length
-      // sound contrived? It is... 😅
+      [type.toUpperCase() + type.length]: description,
     }
   }
 
@@ -39,11 +37,8 @@ test('can use expressions as property names', () => {
 })
 
 //////// Elaboration & Feedback /////////
-/*
-http://ws.kcd.im/?ws=ES6+and+Beyond&e=Object+Literals&em=
-*/
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true
   expect(true).toBe(submitted)
 })
 ////////////////////////////////

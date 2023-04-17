39e97158a5012114729fd51cf2891963adaf534b
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-16 23:56:34 -0400

    Update 12_maps.test.js

--- exercises/12_maps.test.js
+++ exercises/12_maps.test.js
@@ -1,6 +1,6 @@
 test('has a set method', () => {
-  // Create a new map called 'myMap'
-  // add a new entry. Use 'name' as the key and 'Aaron' as the value
+  const myMap = new Map()
+  myMap.set('name', 'Aaron')
 
   expect(myMap.get('name')).toBe('Aaron')
 })
@@ -9,8 +9,8 @@ test('can use objects as a key', () => {
   const user = {name: 'Aaron'}
   const value = {twitter: '@js_dev', gplus: '+AaronFrost'}
 
-  // Create a map called 'myMap'
-  // add a new entry. Use user as the key, and value as the value
+  const myMap = new Map()
+  myMap.set(user, value)
 
   expect(myMap.has(user)).toBe(true)
   expect(myMap.get(user)).toBe(value)
@@ -19,17 +19,14 @@ test('can use objects as a key', () => {
 test(`doesn't coerce keys`, () => {
   const myMap = new Map()
   myMap.set(1, 'Aaron')
-  expect(myMap.get('1')).toBe(/*ENTER YOUR GUESS HERE*/)
+  expect(myMap.get('1')).toBe(undefined)
   myMap.set('1', 'Aaron')
-  expect(myMap.get('1')).toBe(/*ENTER YOUR GUESS HERE*/)
+  expect(myMap.get('1')).toBe('Aaron')
 })
 
 //////// Elaboration & Feedback /////////
-/*
-http://ws.kcd.im/?ws=ES6+and+Beyond&e=Maps&em=
-*/
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true
   expect(true).toBe(submitted)
 })
 ////////////////////////////////

09c86a31496adf1e2a61d4b10b74c16c66c84b3a
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-18 23:18:42 -0400

    Update 19_symbols.test.js

--- exercises/19_symbols.test.js
+++ exercises/19_symbols.test.js
@@ -1,27 +1,32 @@
 test('creating symbols', () => {
-  const symbol = 'I wanna be a symbol one day'
+  const symbol = Symbol()
   expect(typeof symbol).toBe('symbol')
 })
 
 test('giving a symbol a description', () => {
-  const symbol = Symbol() // give me a label
+  const symbol = Symbol('use the force')
   expect(String(symbol)).toBe('Symbol(use the force)')
 })
 
 test('symbols are unique', () => {
   const s1 = Symbol()
   const s2 = Symbol()
-  expect(s1 === s2).toBe(/* enter your guess here */)
+  expect(s1 === s2).toBe(false)
 
   const s3 = Symbol('I am a symbol')
   const s4 = Symbol('I am a symbol')
-  expect(s3 === s4).toBe(/* enter your guess here */)
+  expect(s3 === s4).toBe(false)
 })
 
 test('symbols on objects', () => {
   const symbol = Symbol('metadata')
-  // make an object called `game` that
-  // makes this test pass
+  const game = {
+    name: 'The Legend of Zelda',
+    releaseDate: 'February 21, 1986',
+    [symbol]: {
+      fans: 'about a billion',
+    },
+  }
 
   expect(JSON.parse(JSON.stringify(game))).toEqual({
     name: 'The Legend of Zelda',
@@ -33,11 +38,8 @@ test('symbols on objects', () => {
 })
 
 //////// Elaboration & Feedback /////////
-/*
-http://ws.kcd.im/?ws=ES6+and+Beyond&e=Symbols&em=
-*/
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true
   expect(true).toBe(submitted)
 })
 ////////////////////////////////

a8cbf9b882910b1e8e549eacc54531161366ce7b
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-18 23:22:11 -0400

    Update 22_reflect.test.js

--- exercises/22_reflect.test.js
+++ exercises/22_reflect.test.js
@@ -6,7 +6,7 @@ test('Reflect.apply can be used to call a function', () => {
     },
   }
 
-  const result = null // use Reflect.apply to invoke person.sayHi
+  const result = Reflect.apply(person.sayHi, person, ['Hey there', 'Jaimee'])
   expect(result).toBe('Hey there Jaimee! My name is Fred')
 })
 
@@ -16,9 +16,8 @@ test('Reflect.deleteProperty can be used instead of the `delete` keyword', () =>
     configurable: false,
     value: 'YOU CANNOT GET RID OF ME!',
   })
-  // use Reflect.deleteProperty to delete the age property from the person object
-  const ageDeleted = delete person.age
-  const protectedDeleted = delete person.protected
+  const ageDeleted = Reflect.deleteProperty(person, 'age')
+  const protectedDeleted = Reflect.deleteProperty(person, 'protected')
   expect(person.age).not.toBeDefined()
   expect(ageDeleted).toBe(true)
   expect(person.protected).toBe('YOU CANNOT GET RID OF ME!')
@@ -39,29 +38,22 @@ test(`Reflect.ownKeys returns the object's own (not inherited) keys (including s
     configurable: false,
     enumerable: false,
   })
-  // hint, the keys will be in the order that they're added to the object
-  // this will be the case for most environments, though it's generally not
-  // a good idea to rely on this fact as it's not specified in the spec.
-  expect(Object.keys(kyle)).toEqual([
-    /* ENTER YOUR GUESS */
-  ])
+  expect(Object.keys(kyle)).toEqual(['awesome'])
   expect(Object.getOwnPropertyNames(kyle)).toEqual([
-    /* ENTER YOUR GUESS */
+    'awesome',
+    'favoriteLanguage',
   ])
-  expect(Object.getOwnPropertySymbols(kyle)).toEqual([
-    /* ENTER YOUR GUESS */
-  ])
+  expect(Object.getOwnPropertySymbols(kyle)).toEqual([favoriteFeature])
   expect(Reflect.ownKeys(kyle)).toEqual([
-    /* ENTER YOUR GUESS */
+    'awesome',
+    'favoriteLanguage',
+    favoriteFeature,
   ])
 })
 
 //////// Elaboration & Feedback /////////
-/*
-http://ws.kcd.im/?ws=ES6+and+Beyond&e=Reflect&em=
-*/
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true
   expect(true).toBe(submitted)
 })
 ////////////////////////////////
